39519ecfd39d7379cd0c9620597db5cb0601cab7
Raman Deep Bajwa <dbajwa763@gmail.com>
2022-06-22 00:42:19 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import Vue from "vue";
+import PhoneMaskInput from "./PhoneMaskInput.vue";
+export default Vue.component('phone-mask-input',PhoneMaskInput);
---
+++ vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    publicPath: "bi-vue-phone-mask"
+}
